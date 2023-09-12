caae5cb4e4ac0fc1b5d5d98e5c11d4b4eb59f4df
DylanJoo <dylanjootw@gmail.com>
2023-09-12 17:42:16 +0800

    [org] move files

--- src/models/utils.py
+++
@@ -1,9 +0,0 @@
-from torch import nn
-
-def get_activation(activation_name):
-    if activation == 'sigmoid':
-        return nn.Sigmoid()
-    elif activation == 'tanh':
-        return nn.Tanh()
-    elif activation == 'relu':
-        return nn.ReLU()
