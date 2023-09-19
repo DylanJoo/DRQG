35868cc99d40e4f22ee314fddb586634d0cac0f3
DylanJoo <dylanjootw@gmail.com>
2023-09-19 22:00:03 +0800

    add peft

---
+++ src/models/peftQG.py
@@ -0,0 +1,2 @@
+
+class peftFlanT5
---
+++ src/run_train_peft.sh
@@ -0,0 +1,20 @@
+TRAIN_FILE=/home/jhju/datasets/nils.sentence.transformers/ce.minilm.hardneg.vL.jsonl
+
+python3 train_peft.py \
+  --model_name_or_path google/flan-t5-base \
+  --tokenizer_name google/flan-t5-base \
+  --config_name google/flan-t5-base \
+  --output_dir models/checkpoints/peft \
+  --max_p_length 256 \
+  --max_q_length 16 \
+  --per_device_train_batch_size 4 \
+  --m_negative_per_example 2 \
+  --m_positive_per_example 2 \
+  --learning_rate 1e-3 \
+  --evaluation_strategy steps \
+  --max_steps 10000 \
+  --save_steps 2000 \
+  --eval_steps 500 \
+  --train_file ${TRAIN_FILE} \
+  --do_train \
+  --do_eval 
