ce1f68f24a676c11f4f5f719a34d2359b691f2f9
DylanJoo <dylanjootw@gmail.com>
2023-11-08 08:40:55 +0800

    add generation config

--- src/run_generation.sh
+++ src/run_generation.sh
@@ -1,13 +1,14 @@
 # scidocs
-for folder in models/checkpoint/flan*$1;do
+for folder in models/checkpoint/$1;do
     name=${folder##*/}
     python3 generate.py \
         --corpus_jsonl ~/datasets/scifact/corpus.jsonl \
-        --model_name models/checkpoint/${name}/checkpoint-20000 \
+        --model_name models/drq/${name}/checkpoint-20000 \
         --tokenizer_name google/flan-t5-base \
         --output_jsonl results/${name}.jsonl \
         --device cuda:0 \
         --num_relevance_scores 10 \
+        --num_relevance_prompt 5 \
         --batch_size 2 \
         --max_length 512 \
         --max_new_tokens 64 \
--- src/test.sh
+++ src/test.sh
@@ -1,3 +1,5 @@
+export CUDA_VISIBLE_DEVICES=2
+
 TRAIN_FILE=/home/jhju/datasets/nils.sentence.transformers/ce.minilm.hardneg.vL.jsonl
 EVAL_FILE=data/ce.minilm.hardneg.vL.eval.small.jsonl
 for m in f1; do
@@ -5,7 +7,7 @@ for m in f1; do
       --model_name_or_path google/flan-t5-base \
       --tokenizer_name google/flan-t5-base \
       --config_name google/flan-t5-base \
-      --output_dir models/checkpoint/flan-t5-base-rel1-ibce-doc-1 \
+      --output_dir models/drqg/flan-t5-base-rel1-ibce-1.25 \
       --max_p_length 128 \
       --max_q_length 16 \
       --per_device_train_batch_size 8 \
@@ -25,7 +27,6 @@ for m in f1; do
       --do_train \
       --do_eval \
       --enable_simlarity_loss "inbatch" \
-      --document_wise_contrastive true \
-      --tau 1 \
-      --run_name ibce-doc-1
+      --tau 1.25 \
+      --run_name rel-1-ibce-1.25
 done
